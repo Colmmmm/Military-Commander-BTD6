aa728c8ff8f5c867b2047c01011c9950545edce0
Colm <84244844+Colmmmm@users.noreply.github.com>
2023-05-02 14:43:08 +0100

    Add files via upload

---
+++ ModHelperData.cs
@@ -0,0 +1,15 @@
+namespace MilitaryCommander;
+
+public static class ModHelperData
+{
+    public const string WorksOnVersion = "36.1";
+    public const string Version = "1.0.0";
+    public const string Name = "Military Commander";
+
+    public const string Description = "Adds the Military commander tower!";
+
+    public const string RepoOwner = "Colmmmm"; // TODO add your github username hero, also in the download url in README.md
+    public const string RepoName = "Military-Commander-BTD6"; // TODO add your repo name here, also in the download url in README.md
+
+    public const string DllName = "MilitaryCommander";
+}
